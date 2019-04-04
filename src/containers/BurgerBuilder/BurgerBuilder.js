24522ebf0612c99f604c520aa6fe9b33791dcda3
theborgh <borghiborghi@gmail.com>
2019-04-04 16:45:05 +0200

    Add initial layout, ingredients css

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends React.Component {
+
+   render() {
+      return (
+         <Aux>
+            <div>
+               Burger
+            </div>
+            <div>
+               Build controls
+            </div>
+         </Aux>
+      );
+   }
+
+}
+
+export default BurgerBuilder;
