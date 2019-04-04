47921c2587c179b5e4b06c2e18a1d42843dc8321
theborgh <borghiborghi@gmail.com>
2019-04-04 17:53:23 +0200

    Add dynamic ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,22 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.css';
 
 const burger = props => {
+   let transformedIngredients = Object.keys(props.ingredients)
+      .map(ingrKey => {
+         return [...Array(props.ingredients[ingrKey])].map((_, i) => 
+            <BurgerIngredient key={ingrKey+i} type={ingrKey} />
+      )})
+         .reduce((arr, el) => {
+            return arr.concat(el);
+         }, []);
+   if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Start adding ingredients!</p>
+   }
+      
    return (
       <div className={classes.Burger}>
          <BurgerIngredient type="bread-top" />
-         <BurgerIngredient type="cheese" />
-         <BurgerIngredient type="meat" />
+         {transformedIngredients}
          <BurgerIngredient type="bread-bottom" />
       </div>
    );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,19 @@ import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Aux';
 
 class BurgerBuilder extends React.Component {
+   state = {
+      ingredients: {
+         salad: 0,
+         bacon: 1,
+         cheese: 3,
+         meat: 0
+      }
+   };
 
    render() {
       return (
          <Aux>
-               <Burger />
+               <Burger ingredients={this.state.ingredients} />
             <div>
                Build controls
             </div>
