fe2d823d5eb27ec6205ec9de0c403ff1acdedab9
theborgh <borghiborghi@gmail.com>
2019-04-05 14:06:53 +0200

    Implement more button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import Burger from '../../components/Burger/Burger';
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+   salad: 0.5,
+   cheese: 0.4,
+   meat: 1.5,
+   bacon: 0.7
+}
 
 class BurgerBuilder extends React.Component {
    state = {
@@ -9,16 +17,30 @@ class BurgerBuilder extends React.Component {
          bacon: 1,
          cheese: 3,
          meat: 0
-      }
+      },
+      totalPrice: 2
    };
+
+   addIngredientHandler = type => {
+      const updatedCount = this.state.ingredients[type] + 1;
+      const updatedIngredients = {
+         ...this.state.ingredients
+      };
+      updatedIngredients[type] = updatedCount;
+      const updatedPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+      this.setState({totalPrice: updatedPrice, ingredients: updatedIngredients});
+   }
+
+   removeIngredientHandler = type => {
+
+   }
 
    render() {
       return (
          <Aux>
-               <Burger ingredients={this.state.ingredients} />
-            <div>
-               Build controls
-            </div>
+            <Burger ingredients={this.state.ingredients} />
+            <BuildControls
+               addedIngredient={this.addIngredientHandler} />
          </Aux>
       );
    }
