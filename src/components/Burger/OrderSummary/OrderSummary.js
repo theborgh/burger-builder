6e56da46a275e303808ee1d92cb33ca75dd9f67b
theborgh <borghiborghi@gmail.com>
2019-04-06 19:24:47 +0200

    Restructure folders

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../../UI/Button/Button';
 import Aux from '../../../hoc/Aux';
 
-const orderSummary = props => {
-   const ingredientsSummary = Object.keys(props.ingredients)
-      .map(ingr => <li key={ingr}><span style={{textTransform: "capitalize"}}>{ingr}:</span> {props.ingredients[ingr]}</li>)
-   return (
-      <Aux>
-         <h3>Your burger:</h3>
-         <ul>
-            {ingredientsSummary}
-         </ul>
-         <p><strong>${props.price.toFixed(2)}</strong></p>
-         <p>Continue to checkout?</p>
-         <Button buttonType="Danger" clicked={props.cancel}>Cancel</Button>
-         <Button buttonType="Success" clicked={props.continue}>Continue</Button>
-      </Aux>
-   )
+class OrderSummary extends Component {
+
+   render () {
+      const ingredientsSummary = Object.keys(this.props.ingredients)
+      .map(ingr => <li key={ingr}><span style={{textTransform: "capitalize"}}>{ingr}:</span> {this.props.ingredients[ingr]}</li>)
+
+      return (
+         <Aux>
+            <h3>Your burger:</h3>
+            <ul>
+               {ingredientsSummary}
+            </ul>
+            <p><strong>${this.props.price.toFixed(2)}</strong></p>
+            <p>Continue to checkout?</p>
+            <Button buttonType="Danger" clicked={this.props.cancel}>Cancel</Button>
+            <Button buttonType="Success" clicked={this.props.continue}>Continue</Button>
+         </Aux>
+      )
+   }
 }
 
-export default orderSummary;
+export default OrderSummary;
