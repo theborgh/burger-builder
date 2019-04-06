ab45a82e90cbdd9335414ffbadabad919ee74127
theborgh <borghiborghi@gmail.com>
2019-04-06 12:58:29 +0200

    Add Sidedrawer responsive design

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,7 +3,7 @@ import classes from './Logo.css'
 import logoImg from '../../assets/images/burger-logo.png'
 
 const logo = props => (
-   <div className={classes.Logo}>
+   <div className={classes.Logo} style={{height: props.height}}>
       <img src={logoImg} alt="Burger Builder" />
    </div>
 );
