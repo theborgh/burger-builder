4eb25923949e0249462d0fa1c0828b7390b10435
theborgh <borghiborghi@gmail.com>
2019-04-18 19:53:30 +0200

    Fix order summary

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = props => (
-   <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
-      <p>Price: <strong>USD 4.40</strong></p>
-   </div>
-);
+const order = props => {
+   const ingredients = [];
+
+   for (let ingredientName in props.ingredients) {
+      ingredients.push({
+         ingredient: ingredientName,
+         amount: props.ingredients[ingredientName]})
+   }
+
+   const ingredientOutput = ingredients.map(ingr => {
+      return <span 
+         key={ingr.ingredient}
+         style={{
+            textTransform: 'capitalize',
+            display: 'inline-block',
+            margin: '0 8px',
+            border: '1px solid #ccc',
+            padding: '5px'
+         }}>
+            {ingr.ingredient} ({ingr.amount})
+      </span>;
+   });
+
+   return (
+      <div className={classes.Order}>
+         <p>Ingredients: {ingredientOutput}</p>
+         <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
+      </div>
+   );
+};
 
 export default order;
