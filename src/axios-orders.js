40fe15a0dac35a18abf475cdfc006a152fc084ea
theborgh <borghiborghi@gmail.com>
2019-04-25 18:08:43 +0200

    Add async redux

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://react-my-burger.firebaseio.com/'
+    baseURL: 'https://react-my-burger-4e92c.firebaseio.com/'
 });
 
 export default instance;
